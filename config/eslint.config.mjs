651872761f08354a403d47013298d8a1c9f677be
Greigh <57759540+Greigh@users.noreply.github.com>
2025-02-22 23:18:17 -0500

    Enhance ESLint configuration for module support; update .gitignore to include environment files; modify package.json scripts for improved linting

--- config/eslint.config.mjs
+++ config/eslint.config.mjs
@@ -1,9 +1,52 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+const { FlatCompat } = require('@eslint/eslintrc');
+const globals = require('globals');
+const pluginJs = require('@eslint/js');
+const { fileURLToPath } = require('url');
+const path = require('path');
 
-/** @type {import('eslint').Linter.Config[]} */
+// Get the directory name of the current module
+const __filename = fileURLToPath(require.main.filename);
+const __dirname = path.dirname(__filename);
+
+// Create an instance of FlatCompat
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: {
+    env: {
+      es6: true,
+      node: true,
+    },
+  },
+});
+
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        myCustomGlobal: 'readonly',
+      },
+      parser: '@babel/eslint-parser',
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    },
+    plugins: {
+      js: pluginJs,
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+    },
+  },
+  ...compat.config({
+    extends: ['eslint:recommended'],
+  }),
 ];
