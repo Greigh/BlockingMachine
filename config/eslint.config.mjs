8eea085d6494af4a256a443b920445fd9fa70601
Daniel <57759540+Greigh@users.noreply.github.com>
2025-02-24 22:27:20 -0500

    Refactor ESLint configuration to use defineConfig; update regex patterns and add TypeScript support; include eslint-define-config dependency

--- config/eslint.config.mjs
+++ config/eslint.config.mjs
@@ -1,11 +1,12 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-const globals = require('globals');
-const pluginJs = require('@eslint/js');
-const { fileURLToPath } = require('url');
-const path = require('path');
+import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { defineConfig } from 'eslint-define-config';
 
 // Get the directory name of the current module
-const __filename = fileURLToPath(require.main.filename);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Create an instance of FlatCompat
@@ -19,34 +20,40 @@ const compat = new FlatCompat({
   },
 });
 
-export default [
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2023,
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        myCustomGlobal: 'readonly',
-      },
-      parser: '@babel/eslint-parser',
-      parserOptions: {
-        requireConfigFile: false,
-        babelOptions: {
-          presets: ['@babel/preset-env'],
-        },
-      },
+export default defineConfig({
+  files: ['**/*.js'],
+  languageOptions: {
+    ecmaVersion: 2023,
+    sourceType: 'module',
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+      myCustomGlobal: 'readonly',
     },
-    plugins: {
-      js: pluginJs,
-    },
-    rules: {
-      'no-unused-vars': 'error',
-      'no-undef': 'error',
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+      requireConfigFile: false,
+      babelOptions: {
+        presets: ['@babel/preset-env'],
+      },
     },
   },
-  ...compat.config({
-    extends: ['eslint:recommended'],
-  }),
-];
+  plugins: {
+    js: pluginJs,
+    '@typescript-eslint': {},
+  },
+  rules: {
+    'no-unused-vars': 'error',
+    'no-undef': 'error',
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+});
